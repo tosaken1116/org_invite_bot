17cc744666f136e5e0f2c4031f50359c3f00d9bf
tosaken1116 <nagato20021122@gmail.com>
2024-01-02 16:07:54 +0900

    refactor: format program

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import discord
 from discord.ext import commands
 import os
@@ -10,28 +9,29 @@ intents = discord.Intents.all()
 
 load_dotenv()
 
-OWNER_NAME =os.getenv('OWNER_NAME')
-INVITE_CHANNEL_NAME = os.getenv('INVITE_CHANNEL_NAME')
-ORG_NAME=os.getenv('ORG_NAME')
-DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-GITHUB_TOKEN=os.getenv('GITHUB_TOKEN')
+OWNER_NAME = os.getenv("OWNER_NAME")
+INVITE_CHANNEL_NAME = os.getenv("INVITE_CHANNEL_NAME")
+ORG_NAME = os.getenv("ORG_NAME")
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
+
 
 @bot.event
 async def on_ready():
-    print(f'Bot is ready. Logged in as {bot.user.name}')
+    print(f"Bot is ready. Logged in as {bot.user.name}")
 
 
 @bot.event
 async def on_raw_reaction_add(payload):
     channel = bot.get_channel(payload.channel_id)
-    if channel.name!=INVITE_CHANNEL_NAME:
+    if channel.name != INVITE_CHANNEL_NAME:
         return
 
     message = await channel.fetch_message(payload.message_id)
     user = message.guild.get_member(payload.user_id)
-    if user.name !=OWNER_NAME:
+    if user.name != OWNER_NAME:
         return
     try:
         res = parse_message(message.content)
@@ -42,25 +42,31 @@ async def on_raw_reaction_add(payload):
         await channel.send(f"招待に失敗しました <@{user.id}> cause:{str(e)}")
 
 
-def parse_message(message)->object:
+def parse_message(message) -> object:
     split_message = message.split(":")
-    if split_message[0]!="id":
+    if split_message[0] != "id":
         raise Exception("invalid format")
-    return {"user_id":split_message[1]}
+    return {"user_id": split_message[1]}
 
 
 def invite_to_org(user_name):
-    res =  requests.get(f"https://api.github.com/users/{user_name}")
+    res = requests.get(f"https://api.github.com/users/{user_name}")
     if res.status_code == 404:
         raise Exception("user not found")
     user_id = res.json()["id"]
-    res = requests.post(f"https://api.github.com/orgs/{ORG_NAME}/invitations",data=json.dumps({
-        "invitee_id":user_id,
-        }),headers={
-            "accept":"application/vnd.github+json",
-            "Authorization":f"Bearer {GITHUB_TOKEN}",
-            "X-GitHub-Api-Version": "2022-11-28"
-        })
+    res = requests.post(
+        f"https://api.github.com/orgs/{ORG_NAME}/invitations",
+        data=json.dumps(
+            {
+                "invitee_id": user_id,
+            }
+        ),
+        headers={
+            "accept": "application/vnd.github+json",
+            "Authorization": f"Bearer {GITHUB_TOKEN}",
+            "X-GitHub-Api-Version": "2022-11-28",
+        },
+    )
     if res.status_code == 201:
         print(f"invite {user_name} Success")
         return
@@ -68,5 +74,6 @@ def invite_to_org(user_name):
         print(f"invite {user_name} Failed  {res.raw}")
         raise Exception(f"Failed to invite {res.raw}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     bot.run(DISCORD_TOKEN)
